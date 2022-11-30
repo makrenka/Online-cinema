db19c4e21698da7ce3c0f866ffec834002f25496
makrenka <smokrenko79@gmail.com>
2022-11-30 14:16:52 +0300

    add web-pack

---
+++ src/core/Component/Component.js
@@ -0,0 +1,39 @@
+export class Component extends HTMLElement {
+    constructor() {
+        super();
+        this.state = {};
+        this.props = {};
+    };
+
+    setState(callback) {
+        this.state = callback(this.state);
+        this.innerHTML = this.render();
+    }
+
+    connectedCallback() {
+        this.innerHTML = this.render();
+        this.componentDidMount();
+        this.registerEvents();
+    }
+
+    disconnectedCallback() {
+        this.componentWillUnMount();
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.componentWillUpdate(name, oldValue, newValue);
+        this.getAttributeNames().forEach(() => {
+            this.props[name] = this.getAttribute(name);
+        })
+    }
+
+    dispatch(type, props) {
+        this.dispatchEvent(new CustomEvent(type, { bubbles:true, detail: props }));
+    }
+
+    registerEvents() {};
+    componentDidMount() {};
+    componentWillUpdate() {};
+    componentWillUnMount() {};
+    render() {};
+}
