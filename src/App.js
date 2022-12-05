3982ede74d48d8afa040f7faccce860afadb170e
makrenka <smokrenko79@gmail.com>
2022-12-05 12:23:14 +0300

    add MovieCard, MovieService an Roter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
-import { Component } from "./core";
+import * as core from "./core";
+import './components'
+import { movieService } from "./services/MovieService";
 
-export class App extends Component {
+export class App extends core.Component {
+  constructor() {
+    super();
 
-    render() {
-        return `
-            <div>My App</div>
-        `
+    this.state = {
+      movies: [],
+      searchValue: '',
+      selectedCategory: ''
     }
+  }
+
+  getMovies() {
+    movieService.getAllMovies()
+    .then(({ data }) => {
+      this.setState((state) => {
+        return {
+          ...state,
+          movies: data
+        }
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    return (
+      `
+      <div id="shell">
+        <it-header></it-header>
+            <main id="main">
+              <it-router>
+                <it-route path="/" component="home-page" title="Home Page"></it-route>
+                <it-route path="/admin" component="admin-page" title="Admin Page"></it-route>
+                <it-route path="/movies/:id" component="movie-page" title="Movie Detail Page"></it-route>
+                <it-route path="*" component="error-page" title="Not Found Page"></it-route>
+                <it-outlet></it-outlet>
+              </it-router>
+            <main>
+          <it-footer></it-footer>
+      </div>
+      `
+    )
+  }
 }
 
-customElements.define('it-app', App);
+customElements.define("my-app", App);
+
+
+
+{/* <it-header></it-header>
+${this.state.movies.map(({ id, title, poster, rating, comments }) => {
+  return `
+    <movie-card 
+      id="${id}"
+      title="${title}"
+      poster="${poster}"
+      rating="${rating}"
+      comments='${JSON.stringify(comments)}'
+    ></movie-card>
+  `
+}).join(' ')} */}
--- src/core/Component/Component.js
+++ src/core/Component/Component.js
@@ -1,50 +1,50 @@
 export class Component extends HTMLElement {
-    constructor() {
-        super();
-        this.state = {};
-        this.props = {};
-        this.isShadow = false;
-    };
+  constructor() {
+    super();
+    this.state = {};
+    this.props = {};
+    this.isShadow = false
+  }
 
-    setState(callback) {
-        this.state = callback(this.state);
-        if(this.isShadow) {
-            this.shadowRoot.innerHTML = this.render();
-        } else {
-            this.innerHTML = this.render();
-        }
-        
+  setState(callback) {
+    this.state = callback(this.state);
+    if(this.isShadow) {
+      this.shadowRoot.innerHTML = this.render()
+    } else {
+      this.innerHTML = this.render()
     }
+  }
 
-    connectedCallback() {
-        if(this.isShadow) {
-            this.attachShadow({ mode: 'open' });
-            this.shadowRoot.innerHTML = this.render();
-        } else {
-            this.innerHTML = this.render();
-        }        
-        this.componentDidMount();
-        this.registerEvents();
+  connectedCallback() {
+    if(this.isShadow) {
+      this.attachShadow({ mode: 'open' });
+      const tml = document.createElement("template");
+      tml.innerHTML = this.render()
+      this.shadowRoot.append(tml.content.cloneNode(true))
+    } else {
+      this.innerHTML = this.render()
     }
+    this.componentDidMount();
+  }
 
-    disconnectedCallback() {
-        this.componentWillUnMount();
-    }
+  disconnectedCallback() {
+    this.componentWillUnmount();
+  }
 
-    attributeChangedCallback(name, oldValue, newValue) {
-        this.componentWillUpdate(name, oldValue, newValue);
-        this.getAttributeNames().forEach(() => {
-            this.props[name] = this.getAttribute(name);
-        })
-    }
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.componentWillUpdate(name, oldValue, newValue);
+    this.getAttributeNames().forEach((name) => {
+      this.props[name] = this.getAttribute(name);
+    });
+  }
 
-    dispatch(type, props) {
-        this.dispatchEvent(new CustomEvent(type, { bubbles:true, detail: props }));
-    }
+  dispatch(type, props) {
+    this.dispatchEvent(new CustomEvent(type, { bubbles: true, detail: props }));
+  }
 
-    registerEvents() {};
-    componentDidMount() {};
-    componentWillUpdate() {};
-    componentWillUnMount() {};
-    render() {};
+  componentDidMount() {}
+  componentWillUnmount() {}
+  componentWillUpdate() {}
+  componentWillUnmount() {}
+  render() {}
 }
